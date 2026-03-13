08e6d30c2db0248dc8568f7069857032fb0f5f4c
Moiz Baloch <khanmoaiz682@gmail.com>
2026-03-13 21:22:42 +0500

    update: Version Updated - Moiz Baloch

--- khushidua/test_qibla.dart
+++ khushidua/test_qibla.dart
@@ -1,8 +1,6 @@
-import 'dart:math' as console;
-
 import 'package:prayers_times/prayers_times.dart';
 
 void main() {
   var coords = Coordinates(31.5497, 74.3436);
-  console.log(Qibla.qibla(coords));
+  print(Qibla.qibla(coords));
 }
